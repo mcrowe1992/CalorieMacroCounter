60ca8ff61577e45e58aaffa08c531458ae187781
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-04-14 19:25:20 -0700

    Routine View Created plus tons of content

--- Calorie Macro Counter/Controllers/HomeController.cs
+++ Calorie Macro Counter/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Calorie_Macro_Counter.Controllers
             return View();
         }
 
+        public IActionResult Routines()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
