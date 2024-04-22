d75b657ad24d2c6698913a9921fa510c11eabbe9
mcrowe1992 <135188706+mcrowe1992@users.noreply.github.com>
2024-04-22 13:24:54 -0700

    added views and hyperlinks for chest and shoulder exercises

--- Calorie Macro Counter/Controllers/HomeController.cs
+++ Calorie Macro Counter/Controllers/HomeController.cs
@@ -38,5 +38,45 @@ namespace Calorie_Macro_Counter.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
-	}
+
+        public IActionResult BenchPress()
+        {
+            return View();
+        }
+
+        public IActionResult InclineDumbbellPress()
+        {
+            return View();
+        }
+
+        public IActionResult BarDips()
+        {
+            return View();
+        }
+
+        public IActionResult StandingCableChestFly()
+        {
+            return View();
+        }
+
+        public IActionResult OverheadPress()
+        {
+            return View();
+        }
+
+        public IActionResult SeatedDumbbellShoulderPress()
+        {
+            return View();
+        }
+
+        public IActionResult DumbbellLateralRaise()
+        {
+            return View();
+        }
+
+        public IActionResult ReverseDumbbellFly()
+        {
+            return View();
+        }
+    }
 }
